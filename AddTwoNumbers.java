2dfaf6d61fce79c6512be363a07c60e8f0ceb2c1
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-12 13:18:16 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,1 +1,11 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, res;
+      res = num2 + num1;
+
+      System.out.println("Sum of these numbers: " +res);
+   }
+}
 
