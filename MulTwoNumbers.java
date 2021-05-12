e7409c99dbcc45069ded32991204782a963f6256
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-12 13:17:26 +0530

    Update MulTwoNumbers.java

--- MulTwoNumbers.java
+++ MulTwoNumbers.java
@@ -1,1 +1,11 @@
+public class MulTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, res;
+      res = num2 * num1;
+
+      System.out.println("Multiplication of these numbers: " +res);
+   }
+}
 
